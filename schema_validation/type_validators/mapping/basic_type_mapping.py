c847f7e4a87f4716be0147defb93353b89fdc41e
Rajiv Patel <rajiv@ometria.com>
2016-04-30 22:57:44 +0100

    Added schema validator mapping for basic types.

---
+++ schema_validation/type_validators/mapping/basic_type_mapping.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from schema_validation.type_validators.custom_type import CustomTypeSchemaValidator
+from schema_validation.type_validators.date_type import DateTypeSchemaValidator
+from schema_validation.type_validators.integer_type import IntegerTypeSchemaValidator
+
+
+BASIC_TYPE_SCHEMA_VALIDATOR_MAPPING = {
+    cls.type: cls for cls in [
+        DateTypeSchemaValidator,
+        IntegerTypeSchemaValidator,
+        CustomTypeSchemaValidator,
+    ]
+}
