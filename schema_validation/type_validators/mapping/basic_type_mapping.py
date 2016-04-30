28b5a6b70ae5e567fa55f4a69cfe93e951828e8f
Rajiv Patel <rajiv@ometria.com>
2016-04-30 23:37:00 +0100

    Have a list of basic type schema validators to be used when creating the mapping.

--- schema_validation/type_validators/mapping/basic_type_mapping.py
+++ schema_validation/type_validators/mapping/basic_type_mapping.py
@@ -5,11 +5,12 @@ from schema_validation.type_validators.date_type import DateTypeSchemaValidator
 from schema_validation.type_validators.integer_type import IntegerTypeSchemaValidator
 from schema_validation.type_validators.string_type import StringTypeSchemaValidator
 
-BASIC_TYPE_SCHEMA_VALIDATOR_MAPPING = {
-    cls.type: cls for cls in [
-        DateTypeSchemaValidator,
-        IntegerTypeSchemaValidator,
-        CustomTypeSchemaValidator,
-        StringTypeSchemaValidator,
-    ]
-}
+
+_BASIC_TYPE_SCHEMA_VALIDATORS = [
+    DateTypeSchemaValidator,
+    IntegerTypeSchemaValidator,
+    CustomTypeSchemaValidator,
+    StringTypeSchemaValidator,
+]
+
+BASIC_TYPE_SCHEMA_VALIDATOR_MAPPING = {cls.type: cls for cls in _BASIC_TYPE_SCHEMA_VALIDATORS}
