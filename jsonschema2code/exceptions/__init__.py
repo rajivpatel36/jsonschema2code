515ac1f5f78affdacc6c91b8c175ea32a5a6feaf
Rajiv Patel <rajiv@ometria.com>
2016-05-01 01:12:00 +0100

    Call base init for custom exception to correctly raise error.

--- jsonschema2code/exceptions/__init__.py
+++ jsonschema2code/exceptions/__init__.py
@@ -5,4 +5,4 @@ class SchemaValidationError(Exception):
     def __init__(self, message, class_name=None, property_name=None):
         class_info = "Class: {class_name}.".format(class_name=class_name) if class_name else ""
         property_info = "Property: {property_name}.".format(property_name=property_name) if property_name else ""
-        self.message = " ".join([message, class_info, property_info])
+        super(SchemaValidationError, self).__init__(" ".join([message, class_info, property_info]))
